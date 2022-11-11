aee476e8dba8f234e81575689768288ae5ce79ae
Decagon <decagon@Decagons-MacBook-Air.local>
2022-11-11 11:22:44 +0100

    hello

---
+++ app1.js
@@ -0,0 +1,21 @@
+// const Circle = require('./circle')
+
+// const circle = new Circle
+
+
+// console.log(circle.area(5));
+// console.log(circle.circumference(5))
+
+console.log(global.__dirname)
+
+
+
+
+
+
+
+
+
+
+
+// server.listen(3000)
---
+++ circle.js
@@ -0,0 +1,11 @@
+class Circle {
+    area(r) {
+        return Math.PI * r * r;
+    }
+
+    circumference (r){
+        return 2 * Math.PI * r;
+    }
+}
+
+module.exports = Circle;
